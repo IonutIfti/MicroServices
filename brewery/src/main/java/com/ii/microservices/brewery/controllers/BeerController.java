fa5ab81a6d3bc6049c650f871f816e612f0ab20d
iiftimiciuc <ionut.iftimiciuc-ext@lexisnexisrisk.com>
2023-03-24 15:58:15 +0200

    Implemented CRUD on Beer and Customer

--- brewery/src/main/java/com/ii/microservices/brewery/controllers/BeerController.java
+++ brewery/src/main/java/com/ii/microservices/brewery/controllers/BeerController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -15,17 +16,34 @@ import java.util.UUID;
 @AllArgsConstructor
 public class BeerController {
     private final BeerService beerService;
+    @GetMapping("/beers")
+    public ResponseEntity<List<BeerDTO>> getAllBeers(){
+        return new ResponseEntity<>(beerService.getAllBeers(), HttpStatus.OK);
+    }
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDTO> getBeerById(@PathVariable(name = "beerId") UUID beerId){
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
     }
 
-    @PostMapping
+    @PostMapping("/createBeer")
     public ResponseEntity<Object> createBeer(@RequestBody BeerDTO beerDTO){
         BeerDTO freshBeer = beerService.saveFreshBeer(beerDTO);
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location", freshBeer.getId().toString());
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/updateBeer/{beerId}")
+    public ResponseEntity<Object> updateBeer(@RequestBody BeerDTO beerDTO,
+                                             @PathVariable(name = "beerId")UUID beerId){
+        beerService.updateBeer(beerId,beerDTO);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/deleteBeer/{beerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable(name="beerId")UUID beerId){
+        beerService.deleteBeer(beerId);
     }
 }
--- brewery/src/main/java/com/ii/microservices/brewery/services/impl/CustomerServiceImpl.java
+++ brewery/src/main/java/com/ii/microservices/brewery/services/impl/CustomerServiceImpl.java
@@ -2,10 +2,13 @@ package com.ii.microservices.brewery.services.impl;
 
 import com.ii.microservices.brewery.models.CustomerDTO;
 import com.ii.microservices.brewery.services.CustomerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 @Service
+@Slf4j
 public class CustomerServiceImpl implements CustomerService {
     @Override
     public CustomerDTO getCustomerById(UUID customerId) {
@@ -16,5 +19,34 @@ public class CustomerServiceImpl implements CustomerService {
                 .email("wick@gmail.com")
                 .age(23)
                 .build();
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomers() {
+        log.info("Getting all customers");
+        return null;
+    }
+
+    @Override
+    public CustomerDTO createCustomer(CustomerDTO customer) {
+        log.info("Created customer with ID: {}",customer.getId());
+        return CustomerDTO.builder()
+                .id(customer.getId())
+                .firstName(customer.getFirstName())
+                .lastName(customer.getLastName())
+                .email(customer.getEmail())
+                .age(customer.getAge())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDTO customer) {
+        log.info("Updated customer with ID: {}",customerId);
+    }
+
+    @Override
+    public void deleteCustomer(UUID customerId) {
+        log.info("Deleted customer with ID: {}",customerId);
+
     }
 }
