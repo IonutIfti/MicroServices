ad4f291427a9eee32151465ce6429bd5a32ff1ea
iiftimiciuc <ionut-nicusor.iftimiciuc@endava.com>
2023-03-27 18:37:14 +0300

    New Logic added to brewery

---
+++ brewery-client/src/test/java/ii/microservices/breweryclient/BreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ii.microservices.breweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BreweryClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
