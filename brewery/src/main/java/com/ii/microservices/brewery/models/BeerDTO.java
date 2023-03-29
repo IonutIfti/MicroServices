53356a40ffd4b348ed1f5be078d9c4cc8cc2e76c
iiftimiciuc <ionut-nicusor.iftimiciuc@endava.com>
2023-03-29 18:04:14 +0300

    Added mappers for Beer and Customer

--- brewery/src/main/java/com/ii/microservices/brewery/models/BeerDTO.java
+++ brewery/src/main/java/com/ii/microservices/brewery/models/BeerDTO.java
@@ -1,10 +1,16 @@
 package com.ii.microservices.brewery.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Version;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.Positive;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.util.UUID;
 
 @Data
@@ -23,5 +29,11 @@ public class BeerDTO {
     private String beerStyle;
     @Positive
     private Long upc;
+    private Long version;
+    private Timestamp createDate;
+    private Timestamp lastModifiedDate;
+    private BigDecimal price;
+    private Integer minOnHand;
+    private Integer quantityToBrew;
 
 }
